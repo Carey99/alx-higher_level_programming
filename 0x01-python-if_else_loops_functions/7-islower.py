49a79e1063ee846625e2225d81d6fe5a13a4179a
Carey99 <edwinakidah1@gmail.com>
2023-11-29 07:27:25 -0800

    Checking lowrcase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    """
-    checks if a character is lowercase.
-
-    Arg:
-    c(str): Character to check.
-
-    Returns:
-    bool: True if lowercase else false.
-    """
-    if 'a' <= c <= 'z':
-        return True
-    else:
-        return False
+    'a' <= c <= 'z'
