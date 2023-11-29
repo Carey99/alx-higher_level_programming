4d84dfeddd86967b06bd6eec2ccc7a2ee1484afd
Carey99 <edwinakidah1@gmail.com>
2023-11-28 22:56:03 -0800

    Converting str to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """
+
+    Prints string in uppercase.
+
+    Arg:
+    c (str): String  to check.
+    """
+    result = ""
+    for i in str:
+        if 'a' <= i <= 'z':
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print(result)
