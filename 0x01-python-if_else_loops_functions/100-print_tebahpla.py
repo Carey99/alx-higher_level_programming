09fcee9d4c37a52da3d0efb781d12275ddc61baa
Carey99 <edwinakidah1@gmail.com>
2023-11-29 14:20:05 -0800

    Swapcase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ mystr = "abcdefghijklmnopqrstuvwxyz"
 
 for i, char in enumerate(mystr[::-1]):
     toggle = char.lower() if i % 2 == 0 else char.upper()
-    print(toggle, end="")
+    print("{}".format(toggle), end="")
 print()
