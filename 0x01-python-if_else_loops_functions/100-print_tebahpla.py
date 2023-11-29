877938543636389b560a74505ba01154a159b380
Carey99 <edwinakidah1@gmail.com>
2023-11-29 14:43:49 -0800

    Swapcase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-mystr = "abcdefghijklmnopqrstuvwxyz"
+start = ord('a')
+end = ord('{')
 
+mystr = "".join(chr(i) for i in range(start, end))
 for i, char in enumerate(mystr[::-1]):
     toggle = char.lower() if i % 2 == 0 else char.upper()
     print("{}".format(toggle), end="")
