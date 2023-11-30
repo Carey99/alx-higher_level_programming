2032a2eb81eede4bff41b58acd98408cc917abe3
Carey99 <edwinakidah1@gmail.com>
2023-11-30 06:07:00 -0800

    Addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    total = a + b
-    print("{:d} + {:d} = {:d}".format(a, b, total))
+from add_0 import add
 
-    from add_0 import add
-    add(1, 2)
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
