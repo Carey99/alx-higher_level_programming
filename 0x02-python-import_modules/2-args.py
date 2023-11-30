38fc00580f123ae126e170a5c9dce3bf97ca9f6f
Carey99 <edwinakidah1@gmail.com>
2023-11-30 11:53:49 -0800

    Passing args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,20 +3,21 @@ import sys
 
 
 def print_arguments():
-
     length = len(sys.argv)
     i = 1
     argnum = length - 1
+
     if length == 1:
         print("{} arguments.".format(argnum))
     elif length == 2:
         print("{} argument:".format(argnum))
     elif length > 1:
         print("{} arguments:".format(argnum))
+
     while i < length:
         print("{}: {}".format(i, sys.argv[i]))
         i += 1
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     print_arguments()
