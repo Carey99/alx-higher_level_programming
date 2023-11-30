a932d11bac14a563789f3c2e6b85417ba0e82752
Carey99 <edwinakidah1@gmail.com>
2023-11-30 11:32:07 -0800

    Arguments printing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-length = len(sys.argv)
-i = 1
-argnum = length - 1
 
-if length == 1:
-    print("{} arguments.".format(argnum))
-if length == 2:
-    print("{} argument:".format(argnum))
-elif length > 1:
-    print("{} arguments:".format(argnum))
+def print_arguments():
 
-while i < length:
-    print("{}: {}".format(i, sys.argv[i]))
-    i += 1
+    length = len(sys.argv)
+    i = 1
+    argnum = length - 1
+    if length == 1:
+        print("{} arguments.".format(argnum))
+    elif length == 2:
+        print("{} argument:".format(argnum))
+    elif length > 1:
+        print("{} arguments:".format(argnum))
+    while i < length:
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
+
+if __name__ == "__main__":
+
+    print_arguments()
