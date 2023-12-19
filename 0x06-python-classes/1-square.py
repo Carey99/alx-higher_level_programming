596ccdb3d2f68538b5b1469aba4d1c19b90905c0
Carey99 <edwinakidah1@gmail.com>
2023-12-19 13:01:16 -0800

    Defines a square with private attribute Size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Attributes of a square"""
+
+
+class Square:
+    """Square attributes starts here"""
+
+    def __init__(self, size):
+        """New square.
+        Args:
+            size: Size of square.
+        """
+        self.__size = size
