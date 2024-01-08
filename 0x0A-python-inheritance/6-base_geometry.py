a62c536f5f8bc0d394a8d4ebf76a2a8eb4e2d5c5
Carey99 <edwinakidah1@gmail.com>
2024-01-08 14:17:45 -0800

    PYCODE STYLE

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Based on 5-base_geometry.py"""
 
+
 class BaseGeometry:
     """Empty class"""
-    
+
     def area(self):
         """
         raises an Exception with a message
