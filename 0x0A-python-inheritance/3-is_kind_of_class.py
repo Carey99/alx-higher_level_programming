769063432c01db6bbfaf880de188f62bc83e27a3
Carey99 <edwinakidah1@gmail.com>
2024-01-08 13:28:50 -0800

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""True if the object is an instance of"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return a bool value if true or not
+    """
+    return isinstance(obj, a_class)
