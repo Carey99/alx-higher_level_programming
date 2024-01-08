1c8264099a50494edeb84f2a1f06574c2ab2a071
Carey99 <edwinakidah1@gmail.com>
2024-01-08 13:41:59 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Checking if inherited directly or indirectly"""
 
+
 def inherits_from(obj, a_class):
     """
     Returns true or false
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return type(obj) is not a_class and isinstance(obj, a_class)
