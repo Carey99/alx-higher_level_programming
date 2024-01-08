cfac8139306bbe3d7df7c352c022fbb53143c861
Carey99 <edwinakidah1@gmail.com>
2024-01-08 14:30:07 -0800

    Validates value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Based on 6-base_geometry.py"""
+
+
+
+class BaseGeometry:
+    """
+    Base cgeo definition
+    """
+
+    def area(self):
+        """
+        not implemented
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        Validates value
+        """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
