5cdd0a638c633b27d6c74b2a338134f81bdddca0
Carey99 <edwinakidah1@gmail.com>
2024-03-28 19:55:09 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Takes a letter a sends a POST request"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    try:
+        r = requests.post(url, data=data)
+        json_r = r.json()
+
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+    except ValueError:
+        print("Not a valid JSON")
