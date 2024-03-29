496aa0e5881b028f31a52f7cacc2385d36dcb5be
Carey99 <edwinakidah1@gmail.com>
2024-03-28 18:43:09 -0700

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using urllib.error method"""
+import urllib.error
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
