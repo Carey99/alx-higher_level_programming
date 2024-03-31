5d34fa89cc955248ad19935f527acf97a8c6855d
Carey99 <edwinakidah1@gmail.com>
2024-03-31 05:19:36 -0700

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""Display GitHub user id using Basic Authentication"""
+"""Displays users GitHub id"""
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
     password = sys.argv[2]
 
-    url = 'https://api.github.com/user'
-    try:
-        response = requests.get(url, auth=(username, password))
-        response.raise_for_status()
-
-        user_data = response.json()
-
-        print("User ID:", user_data['id'])
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    if response.status_code == 200:
+        data = response.json()
+        print(data['id'])
